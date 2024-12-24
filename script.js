56f2be9ad184b29a2d73708965fd39c13a443a97
Anas Raheem <anasraheem.com@gmail.com>
2024-12-24 10:17:05 +0500

    Anadn

---
+++ script.js
@@ -0,0 +1,7 @@
+// Scrollable stories section
+$(document).ready(function() {
+    $(".stories-section").scroll(function() {
+        let scrollLeft = $(this).scrollLeft();
+        // Add any scroll effect if needed
+    });
+});
